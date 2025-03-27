b2c4bebe7748552d816275fb46cb9c6102bb9d69
nifeesleman <nsr-1868-14@amu.edu.et>
2025-03-27 15:11:25 +0300

    Add solmate submodule and implement CreateSubscription script for VRF coordination

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "src/Raffle.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
+import {CreateSubscription} from "script/interactions.s.sol";
 
 contract DeployRaffle is Script {
     function run() public {}
@@ -13,6 +14,12 @@ contract DeployRaffle is Script {
         //local -> deploy mocks ,get local config
         //seploia -> get seploia config
         HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+        if (config.subscriptionId == 0) {
+            //create subscription
+            CreateSubscription createSubscription = new CreateSubscription();
+            (config.subscriptionId.config.vrfCoordnator) = createSubscription
+                .createSubscription(config.vrfCoordnator);
+        }
         vm.startBroadcast();
         Raffle raffle = new Raffle(
             config.entranceFee,
---
+++ script/interactions.s.sol
@@ -0,0 +1,48 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import {Script, console} from "forge-std/Script.sol";
+import {Raffle} from "src/Raffle.sol";
+import {HelperConfig} from "script/HelperConfig.s.sol";
+import {VRFCoordinatorV2_5Mock} from "@chainlink/contracts/src/v0.8/vrf/mocks/VRFCoordinatorV2_5Mock.sol";
+
+contract CreateSubscription is Script {
+    function CreateSubscriptionUsingConfig() public returns (uint256, address) {
+        HelperConfig helperConfig = new HelperConfig();
+        address vrfCoordnator = helperConfig.getConfig().vrfCoordnator;
+
+        (uint256 subId, ) = createSubscription(vrfCoordnator);
+        return (subId, vrfCoordnator);
+    }
+
+    function createSubscription(
+        address vrfCoordnator
+    ) public returns (uint256, address) {
+        console.log("Creating subscribtion on chain id", block.chainid);
+        vm.startBroadcast();
+        uint256 subId = VRFCoordinatorV2_5Mock(vrfCoordnator)
+            .createSubscription();
+        vm.stopBroadcast();
+        console.log("your subscription id is :", subId);
+        console.log(
+            "Please update the subscription Id in your Helperconfig.s.sol"
+        );
+        return (subId, vrfCoordnator);
+    }
+
+    function run() public {
+        CreateSubscriptionUsingConfig();
+    }
+}
+
+contract FundSubscription is Script {
+    uint256 public constant FUND_AMOUNT = 3 ether;
+
+    function fundSubscriptionUsingConfig() public {
+        HelperConfig helperConfig = new HelperConfig();
+        address vrfCoordnator = helperConfig.getConfig().vrfCoordnator;
+        address subscriptionId = helperConfig.getConfig().vrfCoordnator;
+    }
+
+    function run() public {}
+}
