27f36fd674567fc0be75e96d5ea3b7ab5a3355d1
nifeesleman <nsr-1868-14@amu.edu.et>
2025-04-03 15:48:42 +0300

    add foundry-devops submodule and enhance DeployRaffle script with subscription funding and consumer addition

--- script/DeployRaffle.s.sol
+++ script/DeployRaffle.s.sol
@@ -4,10 +4,12 @@ pragma solidity ^0.8.19;
 import {Script} from "forge-std/Script.sol";
 import {Raffle} from "src/Raffle.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
-import {CreateSubscription} from "script/interactions.s.sol";
+import {CreateSubscription, FundSubscription, AddConsumer} from "script/interactions.s.sol";
 
 contract DeployRaffle is Script {
-    function run() public {}
+    function run() public {
+        deployContract();
+    }
 
     function deployContract() public returns (Raffle, HelperConfig) {
         HelperConfig helperConfig = new HelperConfig();
@@ -17,7 +19,16 @@ contract DeployRaffle is Script {
         if (config.subscriptionId == 0) {
             //create subscription
             CreateSubscription createSubscription = new CreateSubscription();
-            (config.subscriptionId, config.vrfCoordnator) = createSubscription.createSubscription(config.vrfCoordnator);
+            (config.subscriptionId, config.vrfCoordnator) = createSubscription
+                .createSubscription(config.vrfCoordnator);
+
+            //Fund it !
+            FundSubscription fundSubscription = new FundSubscription();
+            fundSubscription.fundSubscription(
+                config.vrfCoordnator,
+                config.subscriptionId,
+                config.link
+            );
         }
         vm.startBroadcast();
         Raffle raffle = new Raffle(
@@ -29,6 +40,12 @@ contract DeployRaffle is Script {
             config.callbackGasLimit
         );
         vm.stopBroadcast();
+        AddConsumer addConsumer = new AddConsumer();
+        addConsumer.addConsumer(
+            address(raffle),
+            config.vrfCoordnator,
+            config.subscriptionId
+        );
 
         return (raffle, helperConfig);
     }
