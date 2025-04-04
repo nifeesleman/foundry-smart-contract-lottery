8bcb0341bc5a3d64c0519175afbe556c9aec3a84
nifeesleman <nsr-1868-14@amu.edu.et>
2025-04-04 10:40:47 +0300

    add tests for Raffle contract: implement raffleEntered modifier and testPerformUpKeepUpdatesRaffleStateAndEmitsRequestId function

--- test/unit/RaffleTest.t.sol
+++ test/unit/RaffleTest.t.sol
@@ -5,6 +5,7 @@ import {Test} from "forge-std/Test.sol";
 import {Raffle} from "src/Raffle.sol";
 import {DeployRaffle} from "script/DeployRaffle.s.sol";
 import {HelperConfig} from "script/HelperConfig.s.sol";
+import {Vm} from "forge-std/Vm.sol";
 
 contract RaffleTest is Test {
     Raffle public raffle;
@@ -126,6 +127,7 @@ contract RaffleTest is Test {
     }
 
     function testOerformUpkeepRevertsIfCheckUpkeepIsFalse() public {
+        //Arrange
         uint256 currentBalance = 0;
         uint256 numPlayers = 0;
         Raffle.RaffleState rState = raffle.getRaffleState();
@@ -144,5 +146,30 @@ contract RaffleTest is Test {
             )
         );
         raffle.performUpkeep("");
+    }
+
+    modifier raffleEntered() {
+        vm.prank(PLAYER);
+        raffle.enterRaffle{value: entranceFee}();
+        vm.warp(block.timestamp + interval + 1);
+        vm.roll(block.number + 1);
+        _;
+    }
+
+    function testPerformUpKeepUPdatesRaffleStateAndEmitsRequestId()
+        public
+        raffleEntered
+    {
+        //Act
+
+        vm.recordLogs();
+        raffle.performUpkeep("");
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+
+        bytes32 requestId = entries[1].topics[0];
+        //Assert
+        Raffle.RaffleState raffleState = raffle.getRaffleState();
+        assert(uint256(requestId) > 0);
+        assert(uint256(raffleState) == 1);
     }
 }
